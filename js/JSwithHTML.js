dfb7ba0846c6a58298899a1d238341b4f0058b95
Armando Salazar <mando@armandos-mbp.lan>
2020-08-10 15:13:32 -0500

    JS with HTML Lesson

--- js/JSwithHTML.js
+++ js/JSwithHTML.js
@@ -16,11 +16,16 @@ var question = "Do you want to continue?";
 var answer = confirm(question);
 console.log("answer= " + answer);
 
-var question2 = "What is your sign?";
+var question2 = "Please don't answer";
 var answer2 = prompt(question2);
 console.log(answer2);
+console.log(typeof answer2);
 
 var question3 = "What is your favorite number?";
 var answer3 = prompt(question3);
 console.log(answer3);
 console.log(typeof answer3);
+
+var question4 = "If you had to choose your favorite dog, which breed would it be?";
+var answer4 = prompt(question4);
+console.log(answer4);
