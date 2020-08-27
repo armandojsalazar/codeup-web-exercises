c7c6abf0869dbb9d04f75c9c6d8093bc78cbfa5f
armandojsalazar <armando.salazar89@gmail.com>
2020-08-27 17:01:54 -0500

    Maps API

---
+++ js/map-exercise.js
@@ -0,0 +1,94 @@
+
+mapboxgl.accessToken = MAPBOX_TOKEN;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-streets-v11', // stylesheet location
+    center: [-98.4936, 29.4241], // starting position [lng, lat]
+    zoom: 8 // starting zoom
+});
+
+//Favorite #1
+let popupMarker = function () {
+    geocode("13830 I-35, Live Oak, TX 78233", MAPBOX_TOKEN)
+            .then(function(result) {
+                let texasRoadHousePopUp = new mapboxgl.Popup()
+                    .setLngLat([-98.335650, 29.564650])
+                    .setHTML("Texas Roadhouse in Live Oak, TX")
+                let texasRoadHouseMarker = new mapboxgl.Marker()
+                    .setLngLat([-98.335650, 29.564650])
+                    .addTo(map)
+                    .setPopup(texasRoadHousePopUp);
+                texasRoadHouseMarker();
+            });
+}
+let popup1 = document.getElementById('TexasRoad')
+popup1.addEventListener('click', popupMarker);
+
+// Favorite #2
+let popupMarker2 = function(){
+    geocode("2731 S WW White Rd, San Antonio, TX 78222", MAPBOX_TOKEN)
+        .then(function(result) {
+            let smokehousePopup = new mapboxgl.Popup()
+                .setLngLat([-98.405710, 29.382220])
+                .setHTML('2M SmokeHouse San Antonio, TX')
+                // .addTo(map);
+            let twoMSmokehouseMarker = new mapboxgl.Marker()
+                .setLngLat([-98.405710, 29.382220])
+                .addTo(map)
+            .setPopup(smokehousePopup);
+            twoMSmokehouseMarker();
+            // console.log("Geocoded results: " + result);
+            // map.setCenter(result);
+            // map.setZoom(15);
+
+        });
+}
+
+let popup2 = document.getElementById('smokehouse')
+popup2.addEventListener('click', popupMarker2);
+
+//Favorite # 3
+let popupMarker3 = function () {
+    geocode("5999 De Zavala Rd, San Antonio, TX 78249", MAPBOX_TOKEN)
+        .then(function(result) {
+           let romePizzaPopup = new mapboxgl.Popup()
+               .setLngLat([-98.600990, 29.563250])
+               .setHTML("Rome's Pizza San Antonio, TX")
+            let romePizzaMarker = new mapboxgl.Marker()
+                .setLngLat([-98.600990, 29.563250])
+                .addTo(map)
+                .setPopup(romePizzaPopup);
+           romePizzaMarker();
+        });
+
+}
+
+let popup3 = document.getElementById('rome')
+popup3.addEventListener('click', popupMarker3);
+
+let favorite = [
+    {
+    restaurant: "Texas Road House",
+    address: "13830 I-35, Live Oak, TX 78233",
+    entries: ['Ribs, Chicken, Shrimp, Steak'],
+    sides: ['Mashed Potatoes', 'Corn', 'Apple Sauce', 'Green Beans', 'Steak Fries'],
+    appetizers: ['Fried Pickles', 'Onion Bloom', 'Snakebite Rattlers']
+
+},
+    {
+        restaurant: '2M Smokehouse',
+        address: "2731 S WW White Rd, San Antonio, TX 78222",
+        entries: ['Brisket', 'Ribs'],
+        sides: ['Macaroni and Cheese', 'Corn', 'Fries'],
+        appetizers: ['Chips and Salsa', 'Fried Pickles']
+    },
+    {
+        restaurant: 'Rome\'s Pizza',
+        address: "5999 De Zavala Rd, San Antonio, TX 78249",
+        entries: ['Pizza', 'Pazones', 'Salads', 'Wings'],
+        sides: ['Fries', 'Onion Rings'],
+        appetizers: 'None'
+    }
+]
+
+console.log("My favorite Restaurant is " + favorite[1].restaurant + "!")
--- js/maps.js
+++ js/maps.js
@@ -1,1 +1,41 @@
-console.log(secret)
+console.log(secret);
+
+mapboxgl.accessToken = MAPBOX_TOKEN;
+let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: [-98.489615, 29.426827], // starting position [lng, lat]
+        zoom: 14 // starting zoom
+});
+/*
+var alamoMarker = new mapboxgl.Marker()
+    .setLngLat([-98.4861, 29.4260])
+    .addTo(map);
+var alamoPopup = new mapboxgl.Popup()
+    .setLngLat([-98.4861, 29.4260])
+    .setHTML('<p>Remember the Alamo!</p>')
+    .addTo(map);
+
+var codeupPopup = new mapboxgl.Popup()
+    .setLngLat([-98.489615, 29.426827])
+    .setHTML("<p>Codeup Rocks!</p>")
+    .addTo(map);
+
+var codeupMarker = new mapboxgl.Marker()
+    .setLngLat([-98.489615, 29.426827])
+    .addTo(map);
+*/
+
+console.log("Coordinates for Codeup: [-98.489615, 29.426827]");
+geocode("600 Navarro St #350, San Antonio, TX 78205", MAPBOX_TOKEN)
+    .then(function(result) {
+    console.log("Geocoded results: " + result);
+    map.setCenter(result);
+    map.setZoom(15);
+});
+
+reverseGeocode({lng: -98.4861, lat: 29.4260}, MAPBOX_TOKEN)
+    .then(function(results) {
+    // logs the address for The Alamo
+    console.log("Reverse Geocode Results: " + results);
+});
