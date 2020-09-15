a0d7fee8faadadec7ff0c1686f2aa04eeb3ad9c4
armandojsalazar <armando.salazar89@gmail.com>
2020-09-15 10:01:31 -0500

    fizz buzz warm up

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,4 +1,4 @@
-function fizzbuzz(num) {
+/*function fizzbuzz(num) {
     // var fizzbuzz = function (num){
     for(let i = 1;
         i <= num;
@@ -13,7 +13,7 @@ function fizzbuzz(num) {
             console.log(i);
         }
     }
-}
+}*/
 
 
 /*var i = 1;
@@ -31,4 +31,24 @@ while (i <= num) {
     }i++;
 }*/
 
-fizzbuzz(30)
+// fizzbuzz(30)
+
+$().ready(function(){
+    $('#button').click(function (){
+        var num = $('#input').val();
+        for(let i = 1;
+            i <= num;
+            i++){
+            if(i % 15 === 0){
+                $('#output').append('FizzBuzz<br>');
+            }else if(i % 3 === 0) {
+                $('#output').append('Fizz<br>');
+            } else if (i % 5 === 0){
+                $('#output').append('Buzz<br>');
+            }else{
+                $('#output').append(i + '<br>');
+            }
+        }
+        // $('#output').append('')
+    });
+});
