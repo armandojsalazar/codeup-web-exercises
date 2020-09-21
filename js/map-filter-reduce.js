5331fafd5fc904a910ff80ef45baa799d39e2ed5
armandojsalazar <armando.salazar89@gmail.com>
2020-09-21 09:29:16 -0500

    finished the map, filter, reduce exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -35,3 +35,37 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+
+const atLeastThreeLanguages = users.filter((user) => user.languages.length >= 3);
+console.log(atLeastThreeLanguages);
+
+const userEmails = users.map(user => user.email);
+console.log(userEmails);
+
+const totalYearsExp = users.reduce((number, user) => {
+    return number + user.yearsOfExperience;
+}, 0);
+console.log(totalYearsExp);
+
+const averageYearsExp = totalYearsExp / users.length;
+console.log(averageYearsExp);
+
+const longestEmail = users.reduce((str, user) =>{
+    if(user.email.length > str.length){
+        return user.email;
+    } else {
+        return str;
+    }
+},'');
+console.log(longestEmail);
+
+const userNames = users.reduce((str, user) => {
+    return str + user.name + ', '
+}, 'Your instructors are: ');
+console.log(userNames);
+
+const listLanguages = users.reduce((str, user) => {
+    if(user.languages === )
+}, '')
+console.log(listLanguages)
